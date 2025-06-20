58dcb99191ee121672da9bb3abcc0f649e185c9c
xhmyjae <tomatis.mt@gmail.com>
2025-06-20 11:54:02 +0200

    fix(embedding): add embedding function to main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from src.clean import clean_messages
+from src.embedding import create_embeddings_dataframe
 
 # Chargement du CSV
 df = pd.read_csv("data/chat_messages_samueletienne.csv")
@@ -7,7 +8,13 @@ df = pd.read_csv("data/chat_messages_samueletienne.csv")
 # Nettoyage de la colonne 'content'
 df_cleaned = clean_messages(df)
 
+# Embedding
+df_with_embeddings, embeddings = create_embeddings_dataframe(
+    df_cleaned, 
+    model_name='paraphrase-multilingual-MiniLM-L12-v2'
+)
+
 # Sauvegarde propre
-df_cleaned.to_csv("data/chat_messages_samueletienne_clean.csv", index=False)
+df_with_embeddings.to_csv("../data/chat_messages_samueletienne_clean.csv", index=False)
 
 print("✅ Nettoyage terminé")
